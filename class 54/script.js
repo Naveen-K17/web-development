4df805cadf5502c9ebe992f24f1794e23b75e865
Naveen Siddappa Kattimani <naveenkattimani326@gmail.com>
2025-09-04 14:28:30 +0530

    Add files via upload

---
+++ class 54/script.js
@@ -0,0 +1,17 @@
+alert("hello world")
+
+console.log("Code is running........")
+
+var a=prompt("Enter your number")
+var isTrue = confirm("are you sure you want exit")
+
+if(isTrue){
+    console.log("You are exited")
+}
+else{
+    console.log("You are not exited")
+}
+
+console.log("Your number is" + a )
+
+document.title="hey is i am good"
