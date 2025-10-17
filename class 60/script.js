d27a1fbbf0d0fd104763fa09bb54801653d5bd21
Naveen Siddappa Kattimani <naveenkattimani326@gmail.com>
2025-10-17 17:50:53 +0530

    Add files via upload

---
+++ class 60/script.js
@@ -0,0 +1,23 @@
+console.log("My name is naveen");
+let a="naveen";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a.length)
+
+
+let name = "naveen";
+let friend ="paven";
+
+console.log("his name is " +name +" and his friend name is " +friend)
+
+console.log(`His name is ${name} and his friend name is ${friend}`)
+
+let b="NAveen";
+console.log(b.toUpperCase());
+console.log(b.slice(1,4));
+console.log(b.replace("NA" ,"17"));
+console.log(b.concat(a, " rahul"));
